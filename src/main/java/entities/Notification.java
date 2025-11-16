13af59b15af492a087b27534386e3559ec468013
Dimural Murat <dimural722@gmail.com>
2025-11-15 21:00:12 -0500

    notification entity, did not do sendNotification method as that may be for usecase

--- src/main/java/entities/Notification.java
+++ src/main/java/entities/Notification.java
@@ -1,14 +1,34 @@
 package entities;
 
 public class Notification {
-    private int id;
-    private String message;
-    public Notification(int id, String message) {
+    private final int id;
+    private final int userId;
+    private final String message;
+
+    public Notification(int id, int userId, String message) {
         this.id = id;
+        this.userId = userId;
         this.message = message;
+
     }
+
     public int getId() {
         return id;
     }
 
+    public int getUserId() {
+        return userId;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    @Override
+    public String toString() {
+        return message;
+
+    }
 }
+
+
