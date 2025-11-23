ca832a348d4b1c6aeaac93995d8e242ce5b2464e
Anvi S. <anviraj@gmail.com>
2025-11-23 18:33:13 -0500

    done BuildDeckPresenter i think

--- src/main/java/interface_adapters/build_deck/BuildDeckPresenter.java
+++ src/main/java/interface_adapters/build_deck/BuildDeckPresenter.java
@@ -1,8 +1,12 @@
 package interface_adapters.build_deck;
 
+import use_case.build_deck.BuildDeckOutputBoundary;
 import use_case.build_deck.BuildDeckOutputData;
 
-public class BuildDeckPresenter {
+/**
+ * The Presenter for the Build Deck Use Case.
+ */
+public class BuildDeckPresenter implements BuildDeckOutputBoundary{
     private final BuildDeckViewModel viewModel;
 
     public BuildDeckPresenter(BuildDeckViewModel viewModel) {
@@ -11,7 +15,11 @@ public class BuildDeckPresenter {
 
     @Override
     public void prepareSuccessView(BuildDeckOutputData outputData) {
+        // On success, switch to the build deck view
 
+        BuildDeckState buildDeckState = new BuildDeckState();
+        buildDeckState.setDeck(outputData.getDeck());
+        buildDeckState.setErrorMessage(null);
     }
 
     @Override
