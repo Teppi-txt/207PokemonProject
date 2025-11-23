87c0df71d13a1284a7498b378442b7ead43b09f6
Anvi S. <anviraj@gmail.com>
2025-11-23 18:02:47 -0500

    finished BuildDeckState

--- src/main/java/interface_adapters/build_deck/BuildDeckPresenter.java
+++ src/main/java/interface_adapters/build_deck/BuildDeckPresenter.java
@@ -1,4 +1,22 @@
 package interface_adapters.build_deck;
 
+import use_case.build_deck.BuildDeckOutputData;
+
 public class BuildDeckPresenter {
+    private final BuildDeckViewModel viewModel;
+
+    public BuildDeckPresenter(BuildDeckViewModel viewModel) {
+        this.viewModel = viewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(BuildDeckOutputData outputData) {
+
+    }
+
+    @Override
+    public void prepareFailView(String errorMessage){
+        BuildDeckState buildDeckState = new BuildDeckState();
+
+    }
 }
--- src/main/java/interface_adapters/build_deck/BuildDeckState.java
+++ src/main/java/interface_adapters/build_deck/BuildDeckState.java
@@ -1,4 +1,16 @@
 package interface_adapters.build_deck;
 
+import entities.Deck;
+/**
+ * The state for the Build Deck View Model.
+ */
+
 public class BuildDeckState {
+    private Deck deck;
+    private String errorMessage; // if it is null there's no error
+
+    public Deck getDeck() {
+        return deck;
+    }
+    public void setDeck(Deck deck) { this.deck = deck; }
 }
