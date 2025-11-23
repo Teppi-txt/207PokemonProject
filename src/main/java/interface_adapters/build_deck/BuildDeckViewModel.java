03ea0ac237d50c2fbd6e049c93adabef9d2a9b6d
Anvi S. <anviraj@gmail.com>
2025-11-23 18:07:09 -0500

    added BuildDeckViewModel (doesnt extend any ViewModel interface yet)

--- src/main/java/interface_adapters/build_deck/BuildDeckViewModel.java
+++ src/main/java/interface_adapters/build_deck/BuildDeckViewModel.java
@@ -1,4 +1,22 @@
 package interface_adapters.build_deck;
-
+/**
+ * The view model for build deck view.
+ */
 public class BuildDeckViewModel {
+
+    public static final String VIEW_NAME = "build deck";
+
+    private BuildDeckState state = new BuildDeckState();
+
+    public String getViewName() {
+        return VIEW_NAME;
+    }
+
+    public BuildDeckState getState() {
+        return state;
+    }
+
+    public void setState(BuildDeckState state) {
+        this.state = state;
+    }
 }
