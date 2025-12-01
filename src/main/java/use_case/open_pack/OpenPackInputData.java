0ff16be98ac242287865872ca07a9a8b3ff58f35
sara <sarakopilovic@gmail.com>
2025-11-30 21:19:58 -0500

    fixed some sonarqube issues

--- src/main/java/use_case/open_pack/OpenPackInputData.java
+++ src/main/java/use_case/open_pack/OpenPackInputData.java
@@ -2,6 +2,8 @@ package use_case.open_pack;
 
 public class OpenPackInputData {
 
-    public OpenPackInputData() {}
+    public OpenPackInputData() {
+        // nothing needs to be here
+    }
 
 }
